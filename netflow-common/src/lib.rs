ec8c79b55f65fda4e426f47c4e18114e54a46614
s3loy <1915276056@qq.com>
2026-06-09 01:14:58 +0800

    feat(common): add shared FlowKey, FlowStats, FlowEvent types

--- netflow-common/src/lib.rs
+++ netflow-common/src/lib.rs
@@ -1,1 +1,38 @@
 #![no_std]
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct FlowKey {
+    pub src_ip: u32,
+    pub dst_ip: u32,
+    pub src_port: u16,
+    pub dst_port: u16,
+    pub protocol: u8,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default)]
+pub struct FlowStats {
+    pub packets_sent: u64,
+    pub packets_recv: u64,
+    pub bytes_sent: u64,
+    pub bytes_recv: u64,
+    pub ts_start_ns: u64,
+    pub ts_last_ns: u64,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+pub enum FlowEventType {
+    New = 1,
+    Close = 2,
+    Timeout = 3,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+pub struct FlowEvent {
+    pub ty: FlowEventType,
+    pub key: FlowKey,
+    pub stats: FlowStats,
+}
